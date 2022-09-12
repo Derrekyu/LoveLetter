edc9adb0d67d471bcc2d7128a3a1cbae31788064
Derrekyu <derrekyu@gmail.com>
2022-09-12 15:43:46 -0500

    I made this class 9/12.

--- sussytest.java
+++ sussytest.java
@@ -1,5 +1,5 @@
 public class sussytest {
-    sussytest2 d = new sussytest2();
+    static sussytest2 d = new sussytest2();
     public static void main(String args[]){
         System.out.println("bruh");
         d.gay();
